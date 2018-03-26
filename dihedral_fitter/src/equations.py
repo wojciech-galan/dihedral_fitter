3a8ef6049df461a73ad46e5a6442f45d2117baff
wojciech-galan <wojciech.galan@gmail.com>
2018-03-26 16:26:58 +0200

    fixed type annotations in ryckert_bellmans

--- dihedral_fitter/src/equations.py
+++ dihedral_fitter/src/equations.py
@@ -4,10 +4,11 @@
 import math
 from typing import Sequence
 from typing import Union
+from typing import Iterable
 
 
 def ryckaert_bellemans_function(c_parameters: Sequence[Union[int, float]],
-                               phi_angles: Sequence[Union[int, float]]) -> float:
+                                phi_angles: Iterable[Union[int, float]]) -> float:
     """
     Computes Ryckaert-Bellmans function for given C parameters and phi angles using the formula:
     sum_phi(  sum_n_from_0_to_len_c_params( (-1)^n*Cn*cos^n(phi)) ) )
