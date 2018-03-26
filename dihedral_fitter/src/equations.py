6faa426753df953bb7e922bc30a068db4faadc22
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-03-26 11:50:35 +0200

    type anotations and docstring for ryckaert-bellmans

--- dihedral_fitter/src/equations.py
+++ dihedral_fitter/src/equations.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import math
+from typing import Sequence
+from typing import Union
 
 
-def ryckaert_belleman_function(c_parameters, phi_angles):
+def ryckaert_bellemans_function(c_parameters: Sequence[Union[int, float]],
+                               phi_angles: Sequence[Union[int, float]]) -> float:
+    """
+    Computes Ryckaert-Bellmans function for given C parameters and phi angles using the formula:
+    sum_phi(  sum_n_from_0_to_len_c_params( (-1)^n*Cn*cos^n(phi)) ) )
+    """
     return sum(
         sum(math.pow(-1, n) * c_parameters[n] * math.pow(math.cos(phi), n) for n in range(len(c_parameters))) for phi in
         phi_angles)
