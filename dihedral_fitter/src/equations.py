fd8c2e3f6969812a160a7a3f4abd4f8c2e2fddc0
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-03-27 14:23:18 +0200

    cleanup

--- dihedral_fitter/src/equations.py
+++ dihedral_fitter/src/equations.py
@@ -9,16 +9,6 @@ from typing import Iterable
 from typing import List
 
 
-# def ryckaert_bellemans_function(c_parameters: Sequence[Union[int, float]],
-#                                 phi_angles: Iterable[Union[int, float]]) -> float:
-#     """
-#     Computes Ryckaert-Bellmans function for given C parameters and phi angles using the formula:
-#     sum_phi(  sum_n_from_0_to_len_c_params( (-1)^n*Cn*cos^n(phi)) ) )
-#     """
-#     return sum(
-#         sum(math.pow(-1, n) * c_parameters[n] * math.pow(math.cos(phi), n) for n in range(len(c_parameters))) for phi in
-#         phi_angles)
-
 def ryckaert_bellemans_function(c_parameters: Sequence[Union[int, float]],
                                 phi_angles: Iterable[Union[int, float]]) -> float:
     """
