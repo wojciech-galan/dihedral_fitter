dc9db2c85614c85c82d2033c62b827f1fa46ae5c
wojciech-galan <wojciech.galan@gmail.com>
2019-06-04 22:56:03 +0200

    DihedralType class

--- dihedral_fitter/src/reader.py
+++ dihedral_fitter/src/reader.py
@@ -3,7 +3,9 @@
 
 import abc
 import os
+import re
 from typing import Any
+from collections import UserString
 from dihedral_fitter.src.energy_unit import EnergyUnitConverter
 
 
@@ -34,3 +36,44 @@ class EnergyFileReader(FileReader):
 
 class YamlEnergyReader(EnergyFileReader):
     """Assumes the file is in format similar to sample_files/energy.sample.yaml"""
+    pass
+
+
+class DihedralEnergies(object):
+
+    def __init__(self):
+        pass
+
+
+class DihedralType(UserString):
+
+    def __init__(self, string_a:str):
+        found = re.search('([a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z]+)', string_a)
+        improper_dihedral_string_error = RuntimeError("Improper dihedral string: {}".format(string_a))
+        try:
+            group = found.group(1)
+        except AttributeError:
+            raise improper_dihedral_string_error
+        if len(string_a) != len(group):
+            raise improper_dihedral_string_error
+        super().__init__(DihedralType._alphabetic_order(string_a))
+
+    @staticmethod
+    def _alphabetic_order(string_a:str):
+        splitted = string_a.split('-')
+        if splitted[-1] < splitted[0]:
+            return '-'.join(reversed(splitted))
+        return string_a
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+if __name__ == '__main__':
+    print(DihedralType('OS-CT-CT-CT'))
+    print(DihedralType('OS-CT-CT-CT')==DihedralType('CT-CT-CT-OS'))
