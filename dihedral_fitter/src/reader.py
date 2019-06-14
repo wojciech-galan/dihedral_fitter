da122cb18dce25c2e2b87b844b9d089373a88fa1
wojciech-galan <wojciech.galan@uj.edu.pl>
2019-06-14 17:29:48 +0200

    reading yaml - partial implementation

--- dihedral_fitter/src/reader.py
+++ dihedral_fitter/src/reader.py
@@ -39,27 +39,48 @@ class EnergyFileReader(FileReader):
 
 class YamlEnergyReader(EnergyFileReader):
     """Assumes the file is in format similar to sample_files/energy.sample.yaml"""
-    def __init__(self, f_path:str):
+
+    def __init__(self, f_path: str):
         super().__init__(f_path)
 
     def read(self):
         with open(self.path) as f:
             data = yaml.load(f)
-        print(data.keys())
+        return Energies(data['energy unit'], data['angle unit'], data['energies'])
 
 
-class DihedralEnergies(object):
-
-    def __init__(self, energy_unit:str, angle_unit:str, energies:List[Dict[str, Any]]):
+class Energies(object):
+    def __init__(self, energy_unit: str, angle_unit: str, energies: List[Dict[str, Any]]):
         super().__init__()
-        self.energy_unit=  energy_unit
+        self.energy_unit = energy_unit
         self.energy_unit = angle_unit
+        num_of_entries = len(energies[0]["contribution to the particle's energy computed for given angle"])
+        num_of_dihedrals = len(energies)
+        print(energies[0]["contribution to the particle's energy computed for given angle"])
+        print(len(energies[0]["contribution to the particle's energy computed for given angle"]))
+        # zdarza się, że mam dwie wartości energii dla tego samego kąta. Wtedy biorę niższa energię.
+        print([len(x["contribution to the particle's energy computed for given angle"]) == num_of_entries for x in energies])
+        print([len(x["contribution to the particle's energy computed for given angle"]) for x in energies])
+        assert all(len(x["contribution to the particle's energy computed for given angle"]) == num_of_entries for x in energies)
+        self.energies = np.empty((num_of_dihedrals, num_of_entries))
+        self.angles = np.empty((num_of_dihedrals, num_of_entries))
+        self.dihedral_energies_metadata = []
+        for i, entry in enumerate(energies):
+            self.angles[i] = list(entry['energy for given angle'].keys)
+            self.energies[i] = list(entry['energy for given angle'].values)
+            self.dihedral_energies_metadata.append(entry['atom numbers'], entry['dihedral_type'])
 
+
+class DihedralEnergiesMetadata(object):
+    def __init__(self, atom_numbers: List[str], dihedral_type: str):
+        super().__init__()
+        self.atom_numbers = atom_numbers
+        self.dihedral_type = DihedralType(dihedral_type, self.atom_numbers)
 
 
 class DihedralType(UserString):
 
-    def __init__(self, string_a:str):
+    def __init__(self, string_a: str, atom_numbers: List[str]):
         found = re.search('(\w+-\w+-\w+-\w+)', string_a)
         improper_dihedral_string_error = RuntimeError("Improper dihedral string: {}".format(string_a))
         try:
@@ -68,12 +89,13 @@ class DihedralType(UserString):
             raise improper_dihedral_string_error
         if len(string_a) != len(group):
             raise improper_dihedral_string_error
-        super().__init__(DihedralType._alphabetic_order(string_a))
+        super().__init__(DihedralType._alphabetic_order(string_a, atom_numbers))
 
     @staticmethod
-    def _alphabetic_order(string_a:str):
+    def _alphabetic_order(string_a: str, atom_numbers: List[str]):
         splitted = string_a.split('-')
         if splitted[-1] < splitted[0]:
+            atom_numbers.reverse()
             return '-'.join(reversed(splitted))
         return string_a
 
@@ -86,8 +108,7 @@ class DihedralType(UserString):
     def __ne__(self, other):
         return not self.__eq__(other)
 
+
 if __name__ == '__main__':
-    print(DihedralType('OS-CT-CT-CT'))
-    print(DihedralType('OS-CT-CT-CT')==DihedralType('CT-CT-CT-OS'))
     reader = YamlEnergyReader('/home/wojtek/Pobrane/energia_qm.txt')
     reader.read()
