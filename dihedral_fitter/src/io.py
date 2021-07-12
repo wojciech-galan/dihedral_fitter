21b4d1ccf7af196e7d91a1f0508c4bb5e7f245e9
wojciech-galan <wojciech.galan@gmail.com>
2021-07-12 22:08:22 +0200

    bugfixes and reformatting

--- dihedral_fitter/src/io.py
+++ dihedral_fitter/src/io.py
@@ -5,9 +5,10 @@ from typing import List
 from typing import Iterable
 
 
-def load_initial_c_params(params_f_path: str, num_of_params_per_dihedral_type: int, dihedral_types: Iterable[str]) -> np.ndarray:
-    with open(params_f_path):
-        params = json.loads(params_f_path)
+def load_initial_c_params(params_f_path: str, num_of_params_per_dihedral_type: int,
+                          dihedral_types: Iterable[str]) -> np.ndarray:
+    with open(params_f_path) as f:
+        params = json.load(f)
     validate_data_type_in_json(params)
     dihedral_types_in_file_but_not_in_proper_types = set(params) - set(dihedral_types)
     dihedral_types_in_proper_types_but_not_in_file = set(dihedral_types) - set(params)
@@ -19,11 +20,13 @@ In mapping:             {sorted(list(dihedral_types))}'''
         )
     validate_num_of_params_per_dihedral_type(params, num_of_params_per_dihedral_type)
     # return params in the same order as in mapping:
-    sorted_params = {dihedral_type: params[dihedral_type] for dihedral_type in dihedral_types}
-    return np.fromiter(sorted_params.values(), float)
+    ret = np.empty(num_of_params_per_dihedral_type * len(dihedral_types))
+    for i, dihedral_type in enumerate(dihedral_types):
+        ret[i * num_of_params_per_dihedral_type: (i + 1) * num_of_params_per_dihedral_type] = params[dihedral_type]
+    return ret
 
 
-def validate_data_type_in_json(params:Dict[str, List[float]]):
+def validate_data_type_in_json(params: Dict[str, List[float]]):
     if not (isinstance(params, dict) and all(isinstance(v, list) for _, v in params.items())):
         raise RuntimeError(
             '''Your file sgould look like this:
@@ -42,9 +45,11 @@ def validate_num_of_params_per_dihedral_type(params_for_dihedral_types: Dict[str
                                f'type. Correct it for {dihedral_type}.')
 
 
-def save_computed_params(params_array:np.ndarray, dihedral_types:Iterable[str], num_of_params_per_dihedral_type:int, path:str) -> None:
+def save_computed_params(params_array: np.ndarray, dihedral_types: Iterable[str], num_of_params_per_dihedral_type: int,
+                         path: str) -> None:
     out = {}
     for i, dihedral_type in enumerate(dihedral_types):
-        out[dihedral_type] = list(params_array[i*num_of_params_per_dihedral_type: (i+1)*num_of_params_per_dihedral_type])
+        out[dihedral_type] = list(
+            params_array[i * num_of_params_per_dihedral_type: (i + 1) * num_of_params_per_dihedral_type])
     with open(path, 'w') as f:
         json.dump(out, f, indent=4)
